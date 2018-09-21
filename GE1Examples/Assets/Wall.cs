ce6e7438bb57340af832912643076d192006f427
Bryan Duggan <skooter500@gmail.com>
2018-09-21 14:07:43 +0100

    Solution

---
+++ GE1Examples/Assets/Wall.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wall : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        CreateWall(10, 10);
+	}
+
+    void CreateWall(int width, int height)
+    {
+        int halfw = width / 2;
+        float gap = 1.1f;
+        for (int row = 0; row < height; row++)
+        {
+            for (int col = -halfw; col < halfw; col++)
+            {
+                GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                cube.AddComponent<Rigidbody>();
+                float x = col * gap;
+                float y = 0.5f + (row * gap);
+                cube.transform.rotation = Quaternion.identity;
+                cube.transform.position = transform.TransformPoint(new Vector3(x, y, 0));
+                cube.GetComponent<Renderer>().material.color = Color.HSVToRGB(Random.Range(0.0f, 1.0f), 1, 0.8f);
+                cube.transform.parent = this.transform; 
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
