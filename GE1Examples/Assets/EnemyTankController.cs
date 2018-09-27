9d03fad4fbf69d243d453ecd5d74f630e30b556c
Bryan Duggan <skooter500@gmail.com>
2018-09-27 21:38:52 +0100

    bla

--- GE1Examples/Assets/EnemyTankController.cs
+++ GE1Examples/Assets/EnemyTankController.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using System.Text;
+
 
 public class EnemyTankController : MonoBehaviour {
 
@@ -10,25 +10,7 @@ public class EnemyTankController : MonoBehaviour {
     public int current = 0;
     List<Vector3> waypoints = new List<Vector3>();
     public float speed = 10;
-    public Transform player;
-
-    GUIStyle style = new GUIStyle();
-    StringBuilder message = new StringBuilder();
-
-    public void OnGUI()
-    {
-        GUI.color = Color.white;
-        GUI.Label(new Rect(0, 0, Screen.width, Screen.height), "" + message);    
-        if (Event.current.type == EventType.Repaint)
-        {
-            message.Length = 0;
-        }
-    }
-
-    public void Log(string text)
-    {
-        message.Append(text + "\n");
-    }
+    public Transform player;    
 
     public void OnDrawGizmos()
     {
@@ -72,22 +54,22 @@ public class EnemyTankController : MonoBehaviour {
         Vector3 toPlayer = player.position - transform.position;
         if (Vector3.Dot(transform.forward, toPlayer) < 0)
         {
-            Log("Player is behind");
+            GameManager.Log("Player is behind");
         }
         else
         {
-            Log("Player is in front");
+            GameManager.Log("Player is in front");
         }
         float angle = Mathf.Acos(Vector3.Dot(transform.forward, toPlayer) / toPlayer.magnitude) * Mathf.Rad2Deg;
-        Log("Angle to player 1: " + angle);
+        GameManager.Log("Angle to player 1: " + angle);
         //Log("Angle to player 2: " + Vector3.Angle(transform.forward, toPlayer));
         if (angle < 45)
         {
-            Log("Player is inside the FOV");
+            GameManager.Log("Player is inside the FOV");
         }
         else
         {
-            Log("Player is outside the FOV");
+            GameManager.Log("Player is outside the FOV");
         }
     }
 }
