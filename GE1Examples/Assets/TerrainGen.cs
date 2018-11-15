159abb5eb20b9b074c1d5204a76e4524c95b2931
Bryan Duggan <skooter500@gmail.com>
2018-11-15 22:40:47 +0000

    bla

--- GE1Examples/Assets/TerrainGen.cs
+++ GE1Examples/Assets/TerrainGen.cs
@@ -10,23 +10,48 @@ public class TerrainGen : MonoBehaviour {
 
     public float amplitude = 50;
 
+    public Texture2D texture;
+
     Mesh m;
+
+    void MakeTexture()
+    {
+        texture = new Texture2D(numQuads, numQuads);
+        
+        for (int x = 0; x < numQuads; x++)
+        {
+            for (int y = 0; y < numQuads; y++)
+            {
+                texture.SetPixel(x, y, Color.HSVToRGB((x + y) / (numQuads * 2.0f), 1, 1));
+            }
+        }
+        texture.Apply();
+    }
 
     // Use this for initialization
     void Start() {
         MeshFilter mf = gameObject.AddComponent<MeshFilter>();
         MeshRenderer mr = gameObject.AddComponent<MeshRenderer>();
 
+        MakeTexture();
+
+
+
         m = mf.mesh;
 
-        int verticesPerQuad = 6;
+        int verticesPerQuad = 4;
         int vertexCount = verticesPerQuad * numQuads * numQuads;
 
         Vector3[] vertices = new Vector3[vertexCount];
-        int[] triangles = new int[vertexCount];
+        Vector2[] uvs = new Vector2[vertexCount];
+
+        int verticesPerTriangle = 6;
+        int[] triangles = new int[verticesPerTriangle * numQuads * numQuads];
+
 
         Vector3 bottomLeft = new Vector3(-numQuads / 2, 0, -numQuads / 2);
         int vertex = 0;
+        int triangleVertex = 0;
         for (int row = 0; row < numQuads; row++)
         {
             for (int col = 0; col < numQuads; col++)
@@ -39,21 +64,31 @@ public class TerrainGen : MonoBehaviour {
                 int startVertex = vertex;
                 vertices[vertex++] = bl;
                 vertices[vertex++] = tl;
-                vertices[vertex++] = br;
-                vertices[vertex++] = br;
-                vertices[vertex++] = tl;
                 vertices[vertex++] = tr;
+                vertices[vertex++] = br;
 
-                for (int i = 0; i < 6; i++)
-                {
-                    triangles[startVertex + i] = startVertex + i;
-                }                
+                uvs[startVertex] = new Vector2(col / (float) numQuads, row / (float) numQuads);
+                uvs[startVertex + 1] = new Vector2(col / (float) numQuads, (row + 1)/ (float) numQuads);
+                uvs[startVertex + 2] = new Vector2((col + 1) / (float) numQuads, (row + 1) / (float) numQuads);
+                uvs[startVertex + 3] = new Vector2((col + 1)/ (float) numQuads, row / (float) numQuads);
+
+
+                triangles[triangleVertex++] = startVertex;
+                triangles[triangleVertex++] = startVertex + 1;
+                triangles[triangleVertex++] = startVertex + 2;
+
+                triangles[triangleVertex++] = startVertex;
+                triangles[triangleVertex++] = startVertex + 2;
+                triangles[triangleVertex++] = startVertex + 3;
             }
         }
         m.vertices = vertices;
-        m.triangles = triangles;        
+        m.triangles = triangles;
+        m.uv = uvs;
         m.RecalculateNormals();
+        mr.shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.Off;
         mr.material = meshMaterial;
+        mr.material.mainTexture = texture;
 	}
 
     float Map(float a, float b, float c, float d, float e)
@@ -83,6 +118,7 @@ public class TerrainGen : MonoBehaviour {
             vertices[i].y = SampleCell(vertices[i].x, vertices[i].z + t);
         }
         m.vertices = vertices;
+        m.RecalculateNormals();
         t += Time.deltaTime;
 	}
 }
