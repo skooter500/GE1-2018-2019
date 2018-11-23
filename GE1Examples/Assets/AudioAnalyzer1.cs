00460f312e007ef144ab2c83904728857ae9aee0
skooter500 <skooter500@gmail.com>
2018-11-23 16:05:44 +0000

    bla

---
+++ GE1Examples/Assets/AudioAnalyzer1.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+[RequireComponent(typeof(AudioSource))]
+public class AudioAnalyzer1 : MonoBehaviour {
+    AudioSource audioSource;
+    public static int frameSize = 512;
+    public static float[] spectrum;
+    public static float[] bands;
+
+    public bool useMic = false;
+    public AudioClip clip;
+    public string deviceName;
+
+    public AudioMixerGroup amgMain;
+    public AudioMixerGroup amgMic;
+
+
+    // Use this for initialization
+    void Awake () {
+        audioSource = GetComponent<AudioSource>();
+        spectrum = new float[frameSize];
+        bands = new float[(int)Mathf.Log(frameSize, 2)];
+
+        if (useMic)
+        {
+            if (Microphone.devices.Length > 0)
+            {
+                deviceName = Microphone.devices[0].ToString();
+                audioSource.clip = Microphone.Start(deviceName, true, 10, AudioSettings.outputSampleRate);
+                audioSource.outputAudioMixerGroup = amgMic;
+            }
+        }
+        else
+        {
+            audioSource.clip = clip;
+            audioSource.outputAudioMixerGroup = amgMain;
+        }
+        audioSource.Play();
+	}
+
+    void GetBands()
+    {
+        for (int i = 0; i < bands.Length; i++)
+        {
+            int start = (int) Mathf.Pow(2, i) - 1;
+            int width = (int) Mathf.Pow(2, i);
+            int end = start + width;
+            float average = 0;
+            for (int j = start; j < end; j++)
+            {
+                average += spectrum[j] * (j + 1); 
+            }
+            average /= width;
+            bands[i] = average;
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        audioSource.GetSpectrumData(spectrum, 0, FFTWindow.Blackman);
+        GetBands();
+	}
+}
---
+++ GE1Examples/Assets/AudioVisualizer1.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioVisualizer1 : MonoBehaviour {
+    public float radius = 20;
+    List<GameObject> cubes = new List<GameObject>();
+    public float scale = 50;
+	// Use this for initialization
+	void Start () {
+        float theta = (Mathf.PI * 2) / (float) AudioAnalyzer1.bands.Length;
+        for (int i = 0; i < AudioAnalyzer1.bands.Length; i++)
+        {
+            Vector3 p = new Vector3(Mathf.Sin(theta * i) * radius
+                , 0
+                , Mathf.Cos(theta * i) * radius
+                );
+            p = transform.TransformPoint(p);
+            Quaternion q = Quaternion.AngleAxis(theta * i * Mathf.Rad2Deg
+                , Vector3.up
+                );
+            q = transform.rotation * q;
+            GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            cube.transform.SetPositionAndRotation(p, q);
+            cube.GetComponent<Renderer>().material.color = Color.HSVToRGB(
+                i / (float)AudioAnalyzer1.bands.Length
+                , 1
+                , 1
+                );
+            cube.transform.parent = this.transform;
+            cubes.Add(cube);            
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        for (int i = 0; i < cubes.Count; i++)
+        {
+            Vector3 ls = cubes[i].transform.localScale;
+            ls.y = Mathf.Lerp(ls.y, 1 + (AudioAnalyzer1.bands[i] * scale), Time.deltaTime * 5.0f);
+            cubes[i].transform.localScale = ls;
+        }
+	}
+}
